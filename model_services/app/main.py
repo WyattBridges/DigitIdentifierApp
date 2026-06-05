06ce9a0b0303f7a05ce24d5fb1627a190e5759fc
Wyatt Bridges <wdbridge@asu.edu>
2026-06-04 22:52:02 -0700

    Added pydantic typing
    Added typing classes to the fastapi server program in preparation for accepting arrays of data for identification.

--- model_services/app/main.py
+++ model_services/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel, conint, field_validator
-from typing import List
+from pydantic import BaseModel, conint, conlist, field_validator
+from typing import List, Annotated
 import os
 
 app = FastAPI()
@@ -14,6 +14,23 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+Pixel = Annotated[int, conint(ge=0, le=255)]
+
+class Image28x28(BaseModel):
+    pixels: List[List[Pixel]]
+
+    @field_validator("pixels")
+    def check_shape(cls, v):
+        if len(v) != 28:
+            raise ValueError("Image must have 28 rows")
+        for row in v:
+            if len(row) != 28:
+                raise ValueError("Each row must have 28 columns")
+        return v
+
+class PredictionResponse(BaseModel):
+    values: List[float] = conlist(float, min_length = 10, max_length = 10)
 
 # Main Application Endpoint
 @app.get("/", response_class=HTMLResponse)
